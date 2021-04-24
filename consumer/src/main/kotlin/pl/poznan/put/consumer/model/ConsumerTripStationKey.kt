2e956297a0e0a834b1f10f0163a4e4c16e1e9bf2
Adrian Glapiński <adriangl1997@gmail.com>
2021-04-24 23:42:33 +0200

    Add station ID to DayStationAggregateKey

--- consumer/src/main/kotlin/pl/poznan/put/consumer/model/ConsumerTripStationKey.kt
+++ consumer/src/main/kotlin/pl/poznan/put/consumer/model/ConsumerTripStationKey.kt
@@ -6,10 +6,12 @@ import java.time.LocalDateTime
 
 data class ConsumerTripStationKey(
     val eventDateTime: LocalDateTime,
+    val stationId: Int,
     val stationName: String
 ) {
     constructor(tripBicycleStation: TripBicycleStation) : this(
         eventDateTime = tripBicycleStation.trip.dateTime,
+        stationId = tripBicycleStation.bicycleStation.id,
         stationName = tripBicycleStation.bicycleStation.name
     )
 }
--- consumer/src/main/kotlin/pl/poznan/put/consumer/model/DayStationAggregateKey.kt
+++ consumer/src/main/kotlin/pl/poznan/put/consumer/model/DayStationAggregateKey.kt
@@ -4,10 +4,12 @@ import java.time.LocalDate
 
 data class DayStationAggregateKey(
     val day: LocalDate,
+    val stationId: Int,
     val stationName: String
 ) {
     constructor(consumerTripStationKey: ConsumerTripStationKey) : this(
         day = consumerTripStationKey.eventDateTime.toLocalDate(),
+        stationId = consumerTripStationKey.stationId,
         stationName = consumerTripStationKey.stationName
     )
 }
